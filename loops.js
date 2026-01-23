a29b0436a58f67fef8754d44006d12a245139b8d
vidhi28vaghela05 <vidhi28vaghela05@gmail.com>
2026-01-23 13:21:04 +0530

    msg

---
+++ loops.js
@@ -0,0 +1,50 @@
+// loops -- repeating code blocks
+// 1 1 1 1 1
+// 1 2 3 4 5 6
+
+// for loop
+// why use for-loop? when you know how many times you want to repeat a block of code
+// print 1 to 10 -> console.log(1); console.log(2); ... console.log(10);
+// kaya thi javanu chhe -> kaya sudhi javanu chhe -> kevi rite javanu chhe
+// for (ex. 1 -> 50 -> increment by 1)
+// (start; change; change)
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let k = 9; k >= -1; k--) {
+//     console.log(k);
+// }
+
+// for (let j = 1; j <= 10; j++) {
+//     console.log("Hello");
+// }
+// for (let j = 1; j <= 10; j++) {
+//     document.writeln("Hello");
+// }
+
+
+
+
+// while loop -- do while loop
+// kaya thi javanu chhe --> kaya rokavanu chhe --> kevi rite javanu chhe
+// while (ex. 1 -> Hello world -> stop when condition false)
+// start
+// while (end) {
+//   code
+//   change
+// }
+
+let d = 1;
+while (d <= 10) {
+    console.log("While Loop: ", d);
+    d++;
+}
+
+// make it true loops 
+// let k = 50;
+// while(k=20){
+//     console.log(k);
+//     k++
+// }
