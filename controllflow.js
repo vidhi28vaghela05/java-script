b99d4b1d9f3af81c8dde0f89fed8135305ee0561
vidhi28vaghela05 <vidhi28vaghela05@gmail.com>
2026-01-23 12:06:17 +0530

    msg

--- controllflow.js
+++ controllflow.js
@@ -63,3 +63,80 @@ if (loggedin && admin) {
 } else {
     console.log("go to login page");
 }
+
+
+let amount = 5400;
+
+if (amount >= 5000) {
+  console.log("30% discount");
+} 
+else if (amount >= 3000) {
+  console.log("20% discount");
+} 
+else if (amount >= 1000) {
+  console.log("10% discount");
+} 
+else {
+  console.log("No discount");
+}
+
+let isLoggedIn = true;
+let membership = "gold"; // gold, silver, none
+
+if (isLoggedIn && membership === "gold") {
+  console.log("Welcome Gold Member! You get 30% discount.");
+} 
+else if (isLoggedIn && membership === "silver") {
+  console.log("Welcome Silver Member! You get 20% discount.");
+} 
+else if (isLoggedIn) {
+  console.log("Welcome! You get 10% discount.");
+} 
+else {
+  console.log("Please login to get a discount.");
+}
+
+
+// Switch case Statement
+switch ("BOGO") // value -> case value sathe match karshe ane code run thase
+{
+    case "First50":
+        console.log("50% Off on First Order");
+        break;
+
+    case "BOGO":
+        console.log("Buy one get one");
+        break;
+
+    case "BlackFriday":
+        console.log("It's black Friday Sale - get at Rs. 50");
+        break;
+
+    default:
+        console.log("Offer Not Vaild");
+}
+
+
+
+let userRole = "admin";
+
+switch (userRole) {
+    case "admin":
+        console.log("Welcome Admin Dashboard");
+        break;
+
+    case "manager":
+        console.log("Welcome Manager Dashboard");
+        break;
+
+    case "user":
+        console.log("Welcome User Home Page");
+        break;
+
+    case "guest":
+        console.log("Please Login or Register");
+        break;
+
+    default:
+        console.log("Invalid Role");
+}
