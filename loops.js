17ea7a04b62edd9a9303abe8c279f7f86797680c
vidhi28vaghela05 <vidhi28vaghela05@gmail.com>
2026-02-02 12:10:57 +0530

    msg

--- loops.js
+++ loops.js
@@ -120,7 +120,6 @@ for (let k = 1; k <= 201; k++) {
     }
 }
 
-
 // // continue
 for (let a = 1; a <= 10; a++) {
     if (a === 5) {
@@ -129,5 +128,131 @@ for (let a = 1; a <= 10; a++) {
     console.log(a);
 }
 
+// ---------------------------------------------------------------------------
 
-// // for-of, forEach for Arrays
+console.log(" ------------------ Exercises ------------------ ");
+console.log("  ");
+console.log(" example 1 ");
+console.log("  ");
+// example 1   
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+console.log("  ");
+console.log(" example 2 ");
+console.log("  ");
+// example 2
+let ja = 10;
+while (ja >= 1) {
+    console.log(ja);
+    ja--;
+}
+console.log("  ");
+console.log(" example 3 ");
+console.log("  ");
+// example 3
+for (let n = 1; n <= 20; n++) {
+    if (n % 2 === 0) {
+        console.log(n);
+    }
+}
+console.log("  ");
+console.log(" example 4 ");
+console.log("  ");
+// example 4
+let x = 1;
+while (x <= 15) {
+    if (x % 2 !== 0) {
+        console.log(x);
+    }
+    x++;
+}
+console.log("  ");
+console.log(" example 5 ");
+console.log("  ");
+// example 5
+for (let t = 1; t <= 10; t++) {
+    console.log(`5 x ${t} = ${5 * t}`);
+}
+console.log("  ");
+console.log(" example 6 ");
+console.log("  ");
+// example 6
+let sum = 0;
+for (let k = 1; k <= 100; k++) {
+    sum += k;
+}
+console.log(sum);
+console.log("  ");
+console.log(" example 7 ");
+console.log("  ");
+// example 7
+for (let m = 1; m <= 50; m++) {
+    if (m % 3 === 0) {
+        console.log(m);
+    }
+}
+console.log("  ");
+console.log(" example 8 ");
+console.log("  ");
+// example 8
+let num = prompt("Give a number");
+
+for (let p = 1; p <= num; p++) {
+    if (p % 2 === 0) {
+        console.log(p, "is even");
+    } else {
+        console.log(p, "is odd");
+    }
+}
+console.log("  ");
+console.log(" example 9 ");
+console.log("  ");
+// example 9
+let count = 0;
+for (let q = 1; q <= 100; q++) {
+    if (q % 3 === 0 || q % 5 === 0) {
+        count++;
+    }
+}
+console.log(count);
+console.log("  ");
+console.log(" example 10 ");
+console.log("  ");
+// example 10
+for (let r = 1; r <= 100; r++) {
+    console.log(r);
+    if (r % 7 === 0) {
+        break;
+    }
+}
+console.log("  ");
+console.log(" example 11 ");
+console.log("  ");
+// example 11
+for (let s = 1; s <= 20; s++) {
+    if (s % 3 === 0) {
+        continue;
+    }
+    console.log(s);
+}
+console.log("  ");
+console.log(" example 12 ");
+console.log("  ");
+// example 12
+let countOdd = 0;
+
+for (let z = 1; z <= 100; z++) {
+    if (z % 2 === 0) {
+        continue;
+    }
+
+    console.log(z);
+    countOdd++;
+
+    if (countOdd === 5) {
+        break;
+    }
+}
+
+
