d7cb6ee28da21246a9b3a59e4feb2b8b219cf4f9
vidhi28vaghela05 <vidhi28vaghela05@gmail.com>
2026-02-05 13:36:17 +0530

    msg

---
+++ objects.js
@@ -0,0 +1,11 @@
+// Objects vs Array
+// Objects -- when you want to show all data based on
+// one entity(k j entity mate all data jota hoy) (ex.
+// one user data)
+
+// Array -- when you want to show entity (ex. all
+// users)
+
+// create
+// varabile = {}
+// key : value --> key-value sturcture
