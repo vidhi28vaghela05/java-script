d34933bd99d6a0650d249602fd1478c74e720a4d
vidhi28vaghela05 <vidhi28vaghela05@gmail.com>
2026-02-14 11:36:26 +0530

    msg

--- objects.js
+++ objects.js
@@ -35,3 +35,77 @@ user.email;
 // user[demo] --> convert into name (demo = 'name')
 
 // --> output: abc
+
+
+// Nesting and Deep Access
+const user1 = {
+  name: "test",
+  address: {
+    city: "Surat",
+    pin: 395009,
+    locations: {
+      lat: 24.2,
+      lng: 77.4,
+    },
+  },
+  email: "test@gmail.com",
+}; // -- nested Object
+user1.address.locations.lat;  // --> Deep Access
+// user1.address.location.lng;
+
+// Object destructuring
+let { lat, lng } = user1.address.locations;
+let { name, email, address } = user1;
+let { city } = user1.address;
+let { lat: abc_lat } = user1.address.locations;
+
+
+
+
+// destructure and rename variable name
+
+let demo = {
+  last_name: "User",
+  age: 45,
+  email: "demo@user.com",
+};
+
+// Looping (for-in loop)
+// for (variable name in object_name){}
+for (const key in demo) {
+  // console.log(key);
+  // obj.key --> not working
+  // console.log(demo[key]);
+  // console.log(key, ":", demo[key]); //--- log whole object
+}
+
+// Copying Objects: Spred, Object.assign, deep-clone
+// Spred
+// let user_data = { ...demo };
+
+
+let user_data = { gender: "Male", role: "user", ...demo }  // add key-value and copy object
+// Use case: State update
+// const user = {
+//   name: "Amit",
+//   age: 30
+// };
+// const updatedUser = {
+//   ...user,
+//   age: 31
+// };
+// console.log(updatedUser);
+
+// Object.assign — copy and add keys-value
+let user_data_assign = Object.assign({}, demo);  // - nothing will be add just copy
+let user_data_assign1 = Object.assign({phone_number: 7894567894}, demo);  // - add and copy
+// Use case: Default settings + user settings merge
+// const defaultSettings = {
+//   theme: "light",
+//   language: "en"
+// };
+// const userSettings = {
+//   theme: "dark"
+// };
+// const finalSettings = Object.assign({}, defaultSettings, userSettings);
+// console.log(finalSettings);
