bf4d04f9317a3497530c90dccab8f3eef05c79ec
vidhi28vaghela05 <vidhi28vaghela05@gmail.com>
2026-02-16 12:10:06 +0530

    msg

--- dom.js
+++ dom.js
@@ -1,12 +1,10 @@
- // DOM --- Document Object Model
+// DOM --- Document Object Model
 // All Website Code ---> DOM
 // Website Output --> DOM
 // DOM --- HTML body
 
-
 // DOM Tree Structure:
 // Node --> Element (ex. h1, p, b, i), text (ex. lorem), comment
-
 
 // DOM Manipulation - change body or website using
 // select element from html
@@ -16,42 +14,35 @@
 // change attribute
 // event listeners
 
-
-
 //selecting elements
 //getelementbyid
 let a = document.getElementById("abc");
 console.log(a);
 // console.dir(a);
 
-
 // getElementsByClassName
 let b = document.getElementsByClassName("abc");
 console.dir(b);
 
-
 // getElementsByTagName
 let temp_c = document.getElementsByTagName("h1");
 console.dir(temp_c);
-
 
 // querySelector and querySelectorAll
 // most used in project and used by company
 let c = document.querySelector("h1"); // ---- only select first element that find in your html
 console.dir(c);
 
-
 let d = document.querySelector("h1"); // ---- only select first element that find in your html
 console.dir(d);
 
-
 // // Text/Content access: innerText, textContent, innerHTML
-let e = document.querySelector("h1")
+let e = document.querySelector("h1");
 console.dir(e);
 e.innerText = "first h1 tag";
 // document.querySelector("h1").innerText = "first h1 tag"
 
-e.textContent = "h1 tag"
+e.textContent = "h1 tag";
 
 // e.innerHTML = "<i>first h1 tag</i>"
 // e.outerHTML = "<i>h1 with outertext</i>"
@@ -67,17 +58,15 @@ e.textContent = "h1 tag"
 // ----------------------------------------------------
 // Attribute Manipulation
 // getAttribute, setAttribute, removeAttribute
-let link = document.querySelector('a')
-console.dir(link)
+let link = document.querySelector("a");
+console.dir(link);
 // console.log("get Attribute", info) // -- give value of href attribute
 
-link.href = "https://www.google.com/"
+link.href = "https://www.google.com/";
 // element.setAttribute("name", "change")
-link.setAttribute("href", "www.google.com")
+link.setAttribute("href", "www.google.com");
 let info = link.getAttribute("href");
-console.log("get Attribute", info) // -- give value
-
-
+console.log("get Attribute", info); // -- give value
 
 // ------------------------------------
 // Dynamic DOM manipulation
@@ -85,14 +74,38 @@ console.log("get Attribute", info) // -- give value
 // createElement
 let text_h1 = document.createElement("h1");
 console.log(text_h1);
-text_h1.textContent = "create h1 with JS"
+text_h1.textContent = "create h1 with JS";
 
 // append / prepend
 document.body.append(text_h1);
 document.body.prepend(text_h1);
-
 // appendChild
+let outer = document.createElement("div");
+outer.classList.add("outer");
+document.body.append(outer);
+
+let inner = document.createElement("div");
+inner.classList.add("inner");
+outer.appendChild(inner);
 
 // append vs appendChild
 // remove vs removeChild
+
 // ------------------------------------
+// Style Updates via .style and classList (add, remove, toggle)
+console.dir(text_h1);
+text_h1.style.backgroundColor = "black";
+text_h1.style.color = "white";
+text_h1.style.padding = "10px";
+
+// ClassList – add
+let card = document.querySelector(".card");
+console.dir(card);
+card.classList.add("card-style");
+
+// remove
+card.classList.remove("card");
+
+// toggle – class --> available --> remove it
+//        class --> not available --> add it
+card.classList.toggle("card");
