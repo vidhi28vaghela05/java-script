812a03540a8ecbf3234efbfbf9bc82fce04e28cb
vidhi28vaghela05 <vidhi28vaghela05@gmail.com>
2026-02-16 13:28:03 +0530

    msg

--- event.js
+++ event.js
@@ -37,6 +37,15 @@ else{
 }
 })
 
+//input
+//how to know which is typed ?
+let inp =document.querySelector("input");
+inp.addEventListener("input",(dets)=>{
+    if(dets.data!=null){
+
+    console.log(dets.data);
+    }
+})
 
 // ---------------------------------------------------------------------------
 
