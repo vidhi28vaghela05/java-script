2bf482fb41dc8c512432157d0bd1c56881ce55ed
vidhi28vaghela05 <vidhi28vaghela05@gmail.com>
2026-03-03 13:07:18 +0530

    msg

---
+++ this/this.js
@@ -0,0 +1,123 @@
+// this Keyword
+
+// this keyword special keyword in javascript
+
+// beacause of this keyword we can access the properties and methods of an object
+
+// this keyborad is used to refer to the current object
+
+// this keyword is userful when we have to access the properties and methods of an object inside a method of that object
+
+// this keyborad context is determined by how a function is called (that is the reason why this keyword is called dynamic scope)(this ni value this nu use kaya context ma chhe te par depend kare chhe)
+// global Context
+console.log("global", this); // window
+
+// function es5 context
+function abc() {
+  console.log("function", this);
+}
+abc(); // window
+
+// function es6 context
+let bcd = () => {
+  console.log("Fnc ES6", this);
+};
+bcd(); // window
+
+// method context
+// method --> function in object
+
+// 1. method --> fnc es5
+let a = {
+  name: "test",
+  age: 25,
+  write: function () {
+    // method --> ek function je koi object ni under hoy tene
+    // method kevay. // this --> give object
+
+    // if you use arrow function then this key give you window
+    // object because arrow function does not have its own this
+    // keyword it takes this value from its parent scope
+
+    // if you create one more function inside method then this
+    // loose their value --> solution --> you have to create a
+    // arrow function
+
+    console.log("method fnc es5", this);
+  },
+};
+a.write(); // object
+
+// 2. method --> fnc es6
+let b = {
+  name: "test",
+  age: 25,
+  write: () => {
+    console.log("method fnc es6", this);
+  },
+};
+
+b.write(); // window
+
+// 3. method --> fnc es5 ==> fnc es5
+let c = {
+  name: "test",
+  age: 25,
+  write: function () {
+    function abc() {
+      console.log("method fnc es5", this);
+    }
+    abc();
+  },
+};
+
+c.write(); // window
+
+// 4. method --> fnc es6 ==> fnc es6 (arrow function)
+let d = {
+  name: "test",
+  age: 25,
+  write: () => {
+    function abc() {
+      console.log("method fnc es6", this);
+    }
+    abc();
+  },
+};
+
+d.write(); // window
+//event handler context
+document.querySelector("h1").addEventListener("click",function()
+{
+    console.log("Event handler",this);
+}); //this value =html element
+
+//class context
+class Person{
+    constructor(){
+        this.username="test";
+        this.email="test@gmail.com";
+        console.log("class context",this);
+    }
+}
+let p1=new Person();
+console.log(p1);
+
+//context --> this keyword value
+//global context -->window
+//function context -->window
+//method context with es 5 fnc -->object
+//method context with es 6 fnc -->window
+//arrow function inside es5 fnc --> object
+//es5 fnc inside es5 fnc-->window
+//event handler context -->html element
+//class --> blank object
+
+// Manual Binding
+// call, apply, bind
+// function ne call karti vakhate this keyword ni value set karva mate call, apply, bind method no upyog kariye chhiye
+
+let obj5 = {
+  name: "test10",
+  email: "a@a.com",
+}; // save this obj. into this keyword
